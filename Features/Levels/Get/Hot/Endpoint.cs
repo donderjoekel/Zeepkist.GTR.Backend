a7df0f0523a2a79214e65b0734cf6d5f49a07795
Christiaan Bloemendaal <github@tnrd.net>
2023-04-18 20:28:59 +0200

    feat: add levels/hot endpoint

---
+++ Features/Levels/Get/Hot/Endpoint.cs
@@ -0,0 +1,58 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using TNRD.Zeepkist.GTR.Backend.Database;
+using TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+
+namespace TNRD.Zeepkist.GTR.Backend.Features.Levels.Get.Hot;
+
+internal class Endpoint : EndpointWithoutRequest<LevelsGetHotResponseDTO>
+{
+    private readonly GTRContext context;
+
+    /// <inheritdoc />
+    public Endpoint(GTRContext context)
+    {
+        this.context = context;
+    }
+
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get("levels/hot");
+    }
+
+    /// <inheritdoc />
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var list = await context.Records
+            .Where(x => GTRContext.DateTrunc("day", x.DateCreated) == GTRContext.DateTrunc("day", DateTime.Now))
+            .GroupBy(x => x.LevelId)
+            .Select(x => new
+            {
+                LevelId = x.Key,
+                LevelName = x.First().Level.Name,
+                RecordsCount = x.Count()
+            })
+            .OrderByDescending(x => x.RecordsCount)
+            .ToListAsync(ct);
+
+        List<LevelsGetHotResponseDTO.Info> infos = new();
+
+        foreach (var item in list)
+        {
+            LevelsGetHotResponseDTO.Info info = new LevelsGetHotResponseDTO.Info()
+            {
+                RecordsCount = item.RecordsCount,
+                LevelId = item.LevelId,
+                LevelName = item.LevelName
+            };
+        }
+
+        await SendOkAsync(new LevelsGetHotResponseDTO()
+            {
+                Levels = infos
+            },
+            ct);
+    }
+}
