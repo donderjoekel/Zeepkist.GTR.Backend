715636519f593e951af7224859060de44f648f4f
Christiaan Bloemendaal <github@tnrd.net>
2023-04-20 21:12:53 +0200

    refactor: popular endpoint now uses data of whole month and caches result

--- Features/Levels/Get/Popular/Endpoint.cs
+++ Features/Levels/Get/Popular/Endpoint.cs
@@ -1,6 +1,6 @@
-﻿using System.Data.Common;
-using FastEndpoints;
+﻿using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using TNRD.Zeepkist.GTR.Backend.Database;
 using TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
 using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
@@ -10,11 +10,13 @@ namespace TNRD.Zeepkist.GTR.Backend.Features.Levels.Get.Popular;
 internal class Endpoint : EndpointWithoutRequest<LevelsGetPopularResponseDTO>
 {
     private readonly GTRContext context;
+    private readonly IMemoryCache cache;
 
     /// <inheritdoc />
-    public Endpoint(GTRContext context)
+    public Endpoint(GTRContext context, IMemoryCache cache)
     {
         this.context = context;
+        this.cache = cache;
     }
 
     /// <inheritdoc />
@@ -27,13 +29,82 @@ internal class Endpoint : EndpointWithoutRequest<LevelsGetPopularResponseDTO>
     /// <inheritdoc />
     public override async Task HandleAsync(CancellationToken ct)
     {
-        // TODO: Should definitely add caching here
+        if (cache.TryGetValue<List<LevelsGetPopularResponseDTO.Info>>("popular",
+                out List<LevelsGetPopularResponseDTO.Info>? cachedInfos) && cachedInfos != null)
+        {
+            await SendOkAsync(new LevelsGetPopularResponseDTO()
+                {
+                    Levels = cachedInfos
+                },
+                ct);
 
+            return;
+        }
+
+        Dictionary<LevelResponseModel, int> levelRecordsCount = new();
+
+        const int minDay = 1;
+        DateTime now = DateTime.Now;
+        int maxDay = now.Day;
+
+        for (int i = minDay; i < maxDay; i++)
+        {
+            DateTime date = new DateTime(now.Year, now.Month, i);
+            List<LevelsGetPopularResponseDTO.Info> infosForDay = await GetPopularLevelsForDay(date, ct);
+            foreach (LevelsGetPopularResponseDTO.Info info in infosForDay)
+            {
+                if (levelRecordsCount.ContainsKey(info.Level))
+                    levelRecordsCount[info.Level] += info.RecordsCount;
+                else
+                    levelRecordsCount[info.Level] = info.RecordsCount;
+            }
+        }
+
+        List<LevelsGetPopularResponseDTO.Info> infos = new()
+        {
+            new LevelsGetPopularResponseDTO.Info()
+            {
+                Level = new LevelResponseModel()
+                {
+                    Id = 1,
+                    Name = "Test"
+                },
+                RecordsCount = 1
+            }
+        };
+        foreach (KeyValuePair<LevelResponseModel, int> pair in levelRecordsCount)
+        {
+            LevelsGetPopularResponseDTO.Info info = new()
+            {
+                Level = pair.Key,
+                RecordsCount = pair.Value
+            };
+            infos.Add(info);
+        }
+
+        TimeSpan expiry = now.AddDays(1).Date - now;
+        if (expiry > TimeSpan.FromHours(1))
+            expiry = TimeSpan.FromHours(1);
+
+        cache.Set("popular", infos, expiry);
+
+        await SendOkAsync(new LevelsGetPopularResponseDTO()
+            {
+                Levels = infos
+            },
+            ct);
+    }
+
+    private async Task<List<LevelsGetPopularResponseDTO.Info>> GetPopularLevelsForDay(
+        DateTime date,
+        CancellationToken ct
+    )
+    {
         List<LevelsGetPopularResponseDTO.Info> infos = new();
 
         var query = from r in context.Records
             join l in context.Levels on r.Level equals l.Id
-            where r.DateCreated.Value.Month == DateTime.Today.Month
+            where r.DateCreated.Value.Date == date.Date
             group r by l
             into g
             orderby g.Count() descending
@@ -69,10 +140,6 @@ internal class Endpoint : EndpointWithoutRequest<LevelsGetPopularResponseDTO>
             infos.Add(info);
         }
 
-        await SendOkAsync(new LevelsGetPopularResponseDTO()
-            {
-                Levels = infos
-            },
-            ct);
+        return infos;
     }
 }
