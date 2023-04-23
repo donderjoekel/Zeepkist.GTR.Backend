272573d53d6594c3c02fbda953baebf14d87ef12
Christiaan Bloemendaal <github@tnrd.net>
2023-04-23 11:53:43 +0200

    refactor: changed rankings endpoint to be much faster and added caching

--- Features/Users/Rankings/Endpoint.cs
+++ Features/Users/Rankings/Endpoint.cs
@@ -1,19 +1,24 @@
 ﻿using FastEndpoints;
 using FluentResults;
-using TNRD.Zeepkist.GTR.Backend.Directus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using TNRD.Zeepkist.GTR.Backend.Database;
 using TNRD.Zeepkist.GTR.DTOs.Internal.Models;
 using TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
 using TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
 
 namespace TNRD.Zeepkist.GTR.Backend.Features.Users.Rankings;
 
 internal class Endpoint : Endpoint<GenericGetRequestDTO, UsersRankingsResponseDTO>
 {
-    private readonly IDirectusClient client;
+    private readonly GTRContext context;
+    private readonly IMemoryCache cache;
 
-    public Endpoint(IDirectusClient client)
+    public Endpoint(GTRContext context, IMemoryCache cache)
     {
-        this.client = client;
+        this.context = context;
+        this.cache = cache;
     }
 
     /// <inheritdoc />
@@ -26,42 +31,10 @@ internal class Endpoint : Endpoint<GenericGetRequestDTO, UsersRankingsResponseDT
     /// <inheritdoc />
     public override async Task HandleAsync(GenericGetRequestDTO req, CancellationToken ct)
     {
-        Result<List<RecordModel>> getAllWorldRecordsResult = await GetAllWorldRecords(ct);
+        List<UsersRankingsResponseDTO.Ranking> rankings = GetRankings();
 
-        if (getAllWorldRecordsResult.IsFailed)
-        {
-            Logger.LogCritical("Unable to get all world record levels: {Result}", getAllWorldRecordsResult.ToString());
-            await SendAsync(null!, 500, ct);
-            return;
-        }
-
-        List<IGrouping<UserModel, RecordModel>> groupedWorldRecords =
-            getAllWorldRecordsResult.Value
-                .GroupBy(x => x.User.AsT1)
-                .OrderByDescending(x => x.Count())
-                .ToList();
-
-        List<UsersRankingsResponseDTO.Ranking> rankings = new List<UsersRankingsResponseDTO.Ranking>();
-
-        for (int i = 0; i < groupedWorldRecords.Count; i++)
-        {
-            IGrouping<UserModel, RecordModel> grouping = groupedWorldRecords[i];
-
-            rankings.Add(new UsersRankingsResponseDTO.Ranking()
-            {
-                User = new UsersRankingsResponseDTO.UserModel()
-                {
-                    Id = grouping.Key.Id,
-                    SteamId = grouping.Key.SteamId,
-                    SteamName = grouping.Key.SteamName
-                },
-                Position = i + 1,
-                AmountOfWorldRecords = grouping.Count()
-            });
-        }
-
-        int limit = req.Limit.HasValue ? req.Limit.Value : 50;
-        int offset = req.Offset.HasValue ? req.Offset.Value : 0;
+        int limit = req.Limit ?? 50;
+        int offset = req.Offset ?? 0;
 
         if (offset > rankings.Count)
         {
@@ -89,33 +62,53 @@ internal class Endpoint : Endpoint<GenericGetRequestDTO, UsersRankingsResponseDT
             TotalAmount = rankings.Count
         };
 
-        await SendAsync(responseModel, cancellation: ct);
+        await SendOkAsync(responseModel, ct);
     }
 
-    private async Task<Result<List<RecordModel>>> GetAllWorldRecords(CancellationToken ct)
+    private List<UsersRankingsResponseDTO.Ranking> GetRankings()
     {
-        List<RecordModel> worldRecords = new List<RecordModel>();
-
-        int offset = 0;
-        int limit = 100;
-
-        while (true)
+        if (cache.TryGetValue<List<UsersRankingsResponseDTO.Ranking>>("rankings",
+                out List<UsersRankingsResponseDTO.Ranking>? cachedRankings))
         {
-            Result<DirectusGetMultipleResponse<RecordModel>> getResult =
-                await client.Get<DirectusGetMultipleResponse<RecordModel>>(
-                    $"items/records?fields=*.*&filter[is_wr][_eq]=true&offset={offset}&limit={limit}&meta=filter_count",
-                    ct);
-
-            if (getResult.IsFailed)
-                return getResult.ToResult();
-
-            if (!getResult.Value.HasItems)
-                break;
-
-            worldRecords.AddRange(getResult.Value.Data);
-            offset += limit;
+            return cachedRankings!;
         }
 
-        return worldRecords;
+        var wrCounts = from r in context.Records
+            join u in context.Users on r.User equals u.Id
+            where r.IsWr
+            group u by new { u.Id, u.SteamId, u.SteamName }
+            into g
+            orderby g.Count() descending
+            select new
+            {
+                UserId = g.Key.Id,
+                g.Key.SteamId,
+                g.Key.SteamName,
+                WrCount = g.Count()
+            };
+
+        List<UsersRankingsResponseDTO.Ranking> rankings = new();
+
+        int i = 1;
+        foreach (var wrCount in wrCounts)
+        {
+            rankings.Add(new UsersRankingsResponseDTO.Ranking()
+            {
+                Position = i,
+                AmountOfWorldRecords = wrCount.WrCount,
+                User = new UserResponseModel()
+                {
+                    Id = wrCount.UserId,
+                    SteamName = wrCount.SteamName,
+                    SteamId = wrCount.SteamId
+                }
+            });
+
+            i++;
+        }
+
+        cache.Set("rankings", rankings, TimeSpan.FromMinutes(30));
+
+        return rankings;
     }
 }
