d2e2917fe0d2068a710793af061bc4d9cb640365
Christiaan Bloemendaal <github@tnrd.net>
2023-04-28 21:44:14 +0200

    feat: add dummy endpoints for testing authentication

---
+++ Features/Dummy/Private/Endpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+
+namespace TNRD.Zeepkist.GTR.Backend.Features.Dummy.Private;
+
+internal class Endpoint : EndpointWithoutRequest
+{
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Get("dummy/private");
+        Description(b => b.ExcludeFromDescription());
+    }
+
+    /// <inheritdoc />
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendOkAsync(ct);
+    }
+}
