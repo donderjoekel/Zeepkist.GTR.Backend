b0dfe59ddde8923798062854d3d5dfa3b910d63a
Christiaan Bloemendaal <github@tnrd.net>
2023-05-28 16:01:59 +0200

    feat: add RedisOptions

---
+++ Redis/RedisOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TNRD.Zeepkist.GTR.Backend.Redis;
+
+internal class RedisOptions
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string Password { get; set; } = null!;
+}
