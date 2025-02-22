dd128c72d88b60f9fd56d389a275d369ea735385
Christiaan Bloemendaal <github@tnrd.net>
2025-02-22 14:56:05 +0100

    Small tweaks to database creation

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using TNRD.Zeepkist.GTR.Backend.DataStore;
+﻿using Microsoft.Extensions.Options;
+using TNRD.Zeepkist.GTR.Backend.DataStore;
 using TNRD.Zeepkist.GTR.Backend.Jobs;
 
 namespace TNRD.Zeepkist.GTR.Backend;
@@ -6,20 +7,34 @@ namespace TNRD.Zeepkist.GTR.Backend;
 public class Bootstrapper : BackgroundService
 {
     private readonly IJobScheduler _jobScheduler;
+    private readonly JobOptions _jobOptions;
     private readonly IServiceProvider _provider;
+    private readonly ILogger<Bootstrapper> _logger;
 
-    public Bootstrapper(IJobScheduler jobScheduler, IServiceProvider provider)
+    public Bootstrapper(IJobScheduler jobScheduler, IOptions<JobOptions> jobOptions, IServiceProvider provider,
+        ILogger<Bootstrapper> logger)
     {
         _jobScheduler = jobScheduler;
+        _jobOptions = jobOptions.Value;
         _provider = provider;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using IServiceScope scope = _provider.CreateScope();
-        IDatabase database = scope.ServiceProvider.GetRequiredService<IDatabase>();
-        await database.EnsureCreated();
+        if (_jobOptions.EnableWorkshop)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+        else
+        {
+            _logger.LogInformation("Ensuring database created");
+            using IServiceScope scope = _provider.CreateScope();
+            IDatabase database = scope.ServiceProvider.GetRequiredService<IDatabase>();
+            await database.EnsureCreated();
+        }
 
+        _logger.LogInformation("Scheduling recurring jobs");
         _jobScheduler.ScheduleRecurringJobs();
     }
 }
